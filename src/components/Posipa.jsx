58fa722cb3badd4fbae5ff9a2a1c4167a5ef9b2e
Moritz Bunse <mobunse@gmail.com>
2023-05-20 16:35:47 +0200

    Added: Last confirmed vlaue

--- src/components/Posipa.jsx
+++ src/components/Posipa.jsx
@@ -5,10 +5,12 @@ class Posipa extends Component {
     const title = this.props.data.title.rendered;
     const teaser = this.props.data.excerpt.rendered;
     const actionUrl = this.props.data.acf.posipa_pdf;
+    const lastConfirmed = this.props.data.acf.last_confirmed;
     const tags = this.props.data.tags;
     return (
       <div className="posipa" data-tags={tags}>
         <div className="title">{title}</div>
+        <div className="lastConfirmed">{lastConfirmed}</div>
         <div
           className="teaser"
           dangerouslySetInnerHTML={{ __html: teaser }}
